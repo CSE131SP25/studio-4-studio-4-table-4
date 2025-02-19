a25aa273652b995cd2f0d67be7913cbd7aedbd54
SwayingDerp <l.minh@wustl.edu>
2025-02-19 14:14:03 -0600

    almost forgot

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,30 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType=in.next();
+		int red=in.nextInt();
+		int green=in.nextInt();
+		int blue=in.nextInt();
+		boolean isFilled=in.nextBoolean();
+		double x=in.nextDouble();
+		double y=in.nextDouble();
+		double Halfwidth=in.nextDouble();
+		double HalfHeight=in.nextDouble();
+		if(shapeType.equals("rectangle") && isFilled==true){
+			StdDraw.setPenColor(red,green,blue);
+			StdDraw.filledRectangle(x, y, Halfwidth, HalfHeight);
+			}
+		if(shapeType.equals("rectangle") && isFilled==false) {
+			StdDraw.rectangle(x, y, Halfwidth, HalfHeight);
+		}
+		if(shapeType.equals("ellipse") && isFilled==false) {
+			StdDraw.ellipse(x, y, HalfHeight, Halfwidth);
+		}
+		if(shapeType.equals("ellipse") && isFilled==true) {
+			StdDraw.setPenColor(red,green,blue);
+			StdDraw.filledEllipse(x, y, HalfHeight, Halfwidth);
+		}
+
+		}
 	}
-}
+
