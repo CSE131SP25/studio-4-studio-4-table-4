529079da879d0deb69e8a537b1e6f803eda715dc
SwayingDerp <l.minh@wustl.edu>
2025-02-19 14:13:41 -0600

    our country flag is actually a picture of a dude fade with a bald spot, do not question it

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,19 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(255, 175, 175);
+		StdDraw.filledRectangle(50, 50, 50, 50);
+		StdDraw.setPenColor(0, 255, 255);
+		StdDraw.filledEllipse(0.2, 0.2, 0.15, 0.5);
+		StdDraw.filledEllipse(0.6, 0.6, 0.5, 0.15);
+		StdDraw.setPenColor(255, 200, 0);
+		StdDraw.filledRectangle(0.7, 0.7, 0.1, 0.1);
 		
 	}
 }
